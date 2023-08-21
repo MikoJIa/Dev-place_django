f24e1fb6c017a733c88cae2784c03b953376ae41
MikoJIa <Kolya_popov_86@tut.by>
2023-08-21 21:52:43 +0300

    add change apps, admin, index.html, models

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from news.models import *
 
-# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'title',
+                    'category',
+                    'created_at',
+                    'updated_at',
+                    'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'category')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+    ordering = ('id',)
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- news/models.py
+++ news/models.py
@@ -3,20 +3,34 @@ from django.db.models.fields import DateTimeField
 from datetime import datetime
 
 
-
 # id, title, content, created_at, update_at, photo, is_published
 
 class News(models.Model):
     objects = None
-    title = models.CharField("title", max_length=150)
-    content = models.TextField("text", blank=True)
-    created_at = models.DateTimeField("data", auto_now_add=True)
-    updated_at = models.DateTimeField("up_data", auto_now=True)
-    photo = models.ImageField("photo", upload_to='photos/%Y/%m/%d')
-    is_published = models.BooleanField(default=True)
-    # id = models.IntegerField("id", )
+    title = models.CharField(verbose_name="title", max_length=150)
+    content = models.TextField(verbose_name="text", blank=True)
+    created_at = models.DateTimeField(verbose_name="data", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="up_data", auto_now=True)
+    photo = models.ImageField(verbose_name="photo", upload_to='photos/%Y/%m/%d', blank=True)
+    is_published = models.BooleanField(verbose_name="published", default=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категории')
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['id']
 
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='имя котегории')
+
+    class Mate:
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.title
